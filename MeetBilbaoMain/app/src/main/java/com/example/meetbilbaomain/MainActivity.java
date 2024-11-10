e151aff6d4cf6b7413c46b75961fd42c8fd202db
AnderMoreno15 <amorenoch15@gmail.com>
2024-11-10 13:59:51 +0100

    bilbao url finished

--- MeetBilbaoMain/app/src/main/java/com/example/meetbilbaomain/MainActivity.java
+++ MeetBilbaoMain/app/src/main/java/com/example/meetbilbaomain/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.meetbilbaomain;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,23 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageButton ibBilbao = findViewById(R.id.IBBilbao);
+        ibBilbao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear el Intent para abrir la URL
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.bilbao.eus"));
+
+                // Intenta iniciar la actividad para abrir el navegador
+                try {
+                    startActivity(intent);
+                } catch (Exception e) {
+                    // Muestra un Toast si ocurre un error
+                    Toast.makeText(MainActivity.this, "Error al abrir el sitio web", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
